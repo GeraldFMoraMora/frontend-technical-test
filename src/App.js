6e352b599c63b316ac00c3bb9dca1009c667d285
GeraldFMoraMora <geraldmoramora@gmail.com>
2023-12-23 18:33:56 -0600

    reestructura del fichero

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './styles/App.css';
-import TaskList from './components/TaskList';
+import TaskList from './pages/Task/components/TaskList';
 import appLogo from './images/tasklogo.jpg';
-import { FormLogin } from './components/FormLogin';
-import { Home } from './components/Home';
+import { FormLogin } from './pages/LogIn/components/FormLogin';
+import { Home } from './pages/Home/components/Home';
 import { useState } from 'react';
 
 
@@ -16,8 +16,8 @@ function App() {
           src={appLogo}
           className='icon'
         />
-      </div>*/}
-      {/*<div className='task-list-principal'>
+      </div>
+      <div className='task-list-principal'>
         <h1>My task</h1>
         <TaskList/>
       </div>*/}
