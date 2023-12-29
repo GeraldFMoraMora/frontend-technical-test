684b08dc8261a717a46026c5b947215092c2008d
GeraldFMoraMora <geraldmoramora@gmail.com>
2023-12-28 19:07:36 -0600

    Agregado icono para carga de imagen

--- src/pages/TaskAdmin/components/Task.js
+++ src/pages/TaskAdmin/components/Task.js
@@ -1,13 +1,28 @@
 import { AiOutlineCloseCircle, AiFillFileImage } from "react-icons/ai";
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Task.css";
 import TaskService from "../services/TaskService";
+import AvatarEditor from "react-avatar-editor";
 
 function Task({ id, text, onDragStart, onDeleteTask }) {
     const {
         handleDragStart,
         handleDelete,
     } = TaskService(id, text, onDragStart, onDeleteTask);
+
+    const [image, setImage] = useState(null);
+
+    const handleImageUpload = (e) => {
+        const file = e.target.files[0];
+
+        if(file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                setImage(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
 
     return(
         <div className="individual-task-div" draggable onDragStart={handleDragStart}>
@@ -16,10 +31,29 @@ function Task({ id, text, onDragStart, onDeleteTask }) {
             >
                 { text }
             </div>
+            { image && (
+                <AvatarEditor
+                    image={image}
+                    width={40}
+                    height={40}
+                    border={1}
+                    color={[255,255,0,0.6]}
+                    scale={1}
+                />
+            )}
             <div 
                 className="task-img-div-icon"
             >
-                <AiFillFileImage className="img-icon"/>
+                <label htmlFor={`fileInput-${id}`} className="img-icon-label">
+                    <AiFillFileImage className="img-icon" />
+                </label>
+                <input
+                    type="file"
+                    id={`fileInput-${id}`}
+                    accept="image/*"
+                    style={{ display: 'none' }}
+                    onChange={handleImageUpload}
+                />
             </div>
             <div 
                 className="task-delete-div-icon"
@@ -27,6 +61,7 @@ function Task({ id, text, onDragStart, onDeleteTask }) {
             >
                 <AiOutlineCloseCircle className="task-icon"/>
             </div>
+            
         </div>
 
     );
